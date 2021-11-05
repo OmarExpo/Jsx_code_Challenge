60178a3d4282da0f34b51279ad8d2413697299e9
OmarExpo <75645542+OmarExpo@users.noreply.github.com>
2021-11-05 14:02:43 +0100

    initial 1st commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import reactDOM from "react-dom";
+
+reactDOM.render(
+  <div>
+    <h1> Heading</h1>
+    <ul>
+      <li>1st item</li>
+      <li>2nd item</li>
+      <li>3rd item</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
